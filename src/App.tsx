7e65c5489f4b8dd5966cd04742293129396b37e3
Sandeep <sandeep.yadav@technomark.io>
2025-06-26 13:41:06 +0530

    custom progress bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let timer: any = null;
+  const [timeInSecs, setTimeInSecs] = useState<number>(5);
+  const [progressInPercentage, setProgressInPercentage] = useState<number>(0);
+
+  const startProgress = () => {
+    const interval = 100 / timeInSecs / 1000;
+    let counter = 0;
+
+    timer = setInterval(() => {
+      if (counter <= 100) {
+        counter = counter + interval;
+        setProgressInPercentage(counter);
+      } else {
+        counter = 0;
+        clearInterval(timer);
+      }
+    }, interval);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="wrapper">
+          {/* heading */}
+          <div className="headingWrapper">
+            <span></span>
+            <span className="headingText">Custom Progress Bar</span>
+            <span></span>
+          </div>
+          {/* input for getting time in seconds */}
+          <div className="timeInputWrapper">
+            <input
+              className="timeInput"
+              type="number"
+              value={timeInSecs}
+              onChange={(e) => {
+                if (!/^(\s*|\d+)$/.test(e.target.value)) {
+                  return;
+                } else {
+                  setTimeInSecs(parseInt(e.target.value));
+                }
+              }}
+            />
+            <button className="startBtn" onClick={startProgress}>
+              start
+            </button>
+          </div>
+          <div className="customProgress">
+            <span
+              className="progress"
+              style={{
+                width: `${Math.floor(progressInPercentage)}%`,
+              }}
+            ></span>
+            <span className="progressIndicator">
+              {`${Math.floor(progressInPercentage)}`}&nbsp;%
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
